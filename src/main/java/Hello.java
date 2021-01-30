58502bb1dbeafa3bebffc58aaf38a6279efff147
furkan <ozdemir1furkan@gmail.com>
2021-01-30 14:56:36 -0300

    my third commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
-
+        System.out.println("EU is doing great");
+        // forgot to change commit message
     }
 }
